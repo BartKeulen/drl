2947dd2b5573019fd1dd0ac9180e7e9dfcf970f0
akshatbjain <akshatbjain93@gmail.com>
2017-06-26 17:37:19 -0500

    Added comments to DQNNetwork

--- drl/dqn/network.py
+++ drl/dqn/network.py
@@ -23,10 +23,11 @@ options = {
 
 class DQNNetwork(object):
 
-    def __init__(self, num_actions, options_in=None):
+    def __init__(self, n_actions, options_in=None):
         """
         Constructs 'DQNNetwork' object.
 
+            :param n_actions: number of actions
             :param options_in: available and default options for DQNNetwork object:
 
                 'n_conv': 3,                        # Number of convolutional layers
@@ -37,22 +38,46 @@ class DQNNetwork(object):
                 'n_fc': 1,                          # Number of fully-connected layers
                 'fc_units':[512]                    # Number of output units in each fully-connected layer
         """
-        self.num_actions = num_actions
+        self.n_actions = n_actions
 
         if options_in is not None:
             options.update(options_in)
 
         self.print_options()
 
-        self.create_network(num_actions)
+        self.create_network(n_actions)
 
     def weight_variable(self, shape, name, stddev=0.1, mean=0, seed=None):
+        """
+        Generates random initial weights for a layer.
+
+            :param shape: shape of weights
+            :param name: if you want to add a name to recognize which layer this set of weights belong to
+            :param stddev: standard deviation of normal distribution
+            :param mean: mean of normal distribution
+            :param seed: seed for randomness
+            :return: weights
+        """
         return tf.Variable(tf.truncated_normal(shape=shape, stddev=stddev, mean=mean, seed=seed), name=name)
 
     def bias_variable(self, shape, name, value=0.1):
+        """
+        Generates initial bias for a layer.
+
+            :param shape: shape of biases
+            :param name: if you want to add a name to recognize which layer this set of biases belong to
+            :param value: value the biases are set to
+            :return: biases
+        """
         return tf.Variable(tf.constant(value=value, shape=shape), name=name)
 
     def conv2d(self, conv_layer_number):
+        """
+        Creates a 2D convolution layer.
+
+            :param conv_layer_number: layer number of the convolutional layer
+            :return: a convolutional layer
+        """
         # Add weights and biases for each convolutional layer
         self.weights.append(self.weight_variable([self.kernel_sizes[conv_layer_number], self.kernel_sizes[conv_layer_number], self.filters[conv_layer_number], self.filters[conv_layer_number + 1]], 'Conv_Weights_' + str(conv_layer_number + 1)))
         self.biases.append(self.bias_variable([self.filters[conv_layer_number + 1]], 'Conv_Biases_' + str(conv_layer_number + 1)))
@@ -64,6 +89,12 @@ class DQNNetwork(object):
         return conv_layer
 
     def reshape(self):
+        """
+        Reshape or flatten out layer. Used when going from convolutional layers to dense layers.
+
+            :return: flattened output of convolutional layer
+        """
+
         # Reshape for fully connected or dense layer
         conv_shape = self.layers[-1].get_shape().as_list()
         reshape = tf.reshape(self.layers[-1], [-1, conv_shape[1] * conv_shape[2] * conv_shape[3]], name='Flatten_Layer')
@@ -73,6 +104,12 @@ class DQNNetwork(object):
         return reshape
 
     def dense(self, dense_layer_number):
+        """
+        Creates a dense or fully connected layer.
+
+            :param dense_layer_number: layer number of the dense or fully connected layer
+            :return: a dense or fully connected layer
+        """
         # Add weights and biases for each fully connected layer
         self.weights.append(self.weight_variable([self.fc_units[dense_layer_number], self.fc_units[dense_layer_number + 1]], 'FC_Weights_' + str(dense_layer_number + 1)))
         self.biases.append(self.bias_variable([self.fc_units[dense_layer_number + 1]], 'FC_Biases_' + str(dense_layer_number + 1)))
@@ -84,15 +121,24 @@ class DQNNetwork(object):
         return dense
 
     def output_layer(self):
+        """
+        Generates the final or output layer of the network.
+
+            :return: output layer
+        """
         # Weights and biases for Last Hidden Layer
-        self.weights.append(self.weight_variable([self.fc_units[-1], self.num_actions], name='Last_Hidden_Layer_Weights'))
-        self.biases.append(self.bias_variable([self.num_actions], name='Last_Hidden_Layer_Bias'))
+        self.weights.append(self.weight_variable([self.fc_units[-1], self.n_actions], name='Last_Hidden_Layer_Weights'))
+        self.biases.append(self.bias_variable([self.n_actions], name='Last_Hidden_Layer_Bias'))
 
         output = tf.add(tf.matmul(self.layers[-1], self.weights[-1]), self.biases[-1], name='Output_Layer')
 
         return output
 
-    def create_network(self, num_actions):
+    def create_network(self):
+        """
+        Creates a convnet based on user-defined options.
+        If no options are given by user, it by default generates the convnet used by Deepmind for their Atari application.
+        """
 
         # Placeholder for Input image/s
         self.input = tf.placeholder("float", [None, IMAGE_SIZE, IMAGE_SIZE, CHANNELS], name='Input_Layer')
@@ -110,29 +156,41 @@ class DQNNetwork(object):
         # Add channels for 1st layer
         self.filters.insert(0, CHANNELS)
 
+        # Add convolutional layers
         for conv_layer_number in range(options['n_conv']):
             self.layers.append(self.conv2d(conv_layer_number))
 
-        self.layers.append(self.reshape())
+        # Reshape or flatten the last convolutional layer
+        if options['n_conv'] != 0:
+            self.layers.append(self.reshape())
 
+        # Add dense or fully connected layers
         for dense_layer_number in range(options['n_fc']):
             self.layers.append(self.dense(dense_layer_number))
 
+        # Finally add the output layer
         self.layers.append(self.output_layer())
 
+        # Set Q value to output of neural network
         self.set_Q_Value(self.layers[-1])
-        self.set_number_of_layers(len(self.layers) - 3)
 
+        # Set total number of layers in the layer stack
+        self.set_number_of_layers(len(self.layers))
+
+        # Get prediction (highest action value)
         self.predict = tf.argmax(self.get_Q_Value(), 1)
 
         self.target_Q_Value = tf.placeholder(shape=[None], dtype=tf.float32)
         self.actions = tf.placeholder(shape=[None], dtype=tf.int32)
-        self.actions_onehot = tf.one_hot(self.actions, num_actions, dtype=tf.float32)
+        self.actions_onehot = tf.one_hot(self.actions, self.n_actions, dtype=tf.float32)
 
+        # Expected Q value
         self.Q = tf.reduce_sum(tf.multiply(self.Q_value, self.actions_onehot), axis=1)
 
+        # Compute loss
         self.loss = tf.square(self.target_Q_Value - self.Q)
 
+        # Print network summary
         self.print_network_summary('DQNNetwork')
 
     def get_Q_Value(self):
