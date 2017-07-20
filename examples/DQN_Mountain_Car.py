cc9022b0ef4c2e1576d4180dc9c97214bf36ccc0
Akshat <ajain@ihmc.us>
2017-07-19 19:54:40 -0500

    Removed unnecessary code!

--- examples/DQN_Mountain_Car.py
+++ examples/DQN_Mountain_Car.py
@@ -5,46 +5,26 @@ import tensorflow as tf
 import gym
 import sys
 
-from drl.dqn import DQN, DQN_Options
-
-
-def display_error_message():
-	colors = {
-		'red': '\033[91m',
-		'blue': '\033[94m',
-		'reset': '\033[0m'
-	}
-
-	print(
-		colors['red'] + "Incorrect usage. You need to specify the mode (train or test) you want to use this script in.")
-	print("\nCorrect usage:")
-	print(colors['blue'] + "python {} train".format(sys.argv[0]) + colors['red'])
-	print("OR")
-	print(colors['blue'] + "python {} test".format(sys.argv[0]) + colors['reset'])
-	exit()
-
+from drl.dqn import DQN
+from drl.utilities import color_print
 
 if __name__ == '__main__':
 	mode = None
 	try:
 		mode = sys.argv[1]
 	except IndexError:
-		display_error_message()
+		color_print("Incorrect usage. You need to specify the mode (train or test) you want to use this script in.",
+		            color='red', mode='bold')
+		color_print("Correct usage: 'python DQN_Mountain_Car.py train' or 'python DQN_Mountain_Car.py test'",
+		            color='blue')
 
-	env_name = 'MountainCar-v0'
-	env = gym.make(env_name)
+	env = gym.make('MountainCar-v0')
 	n_actions = env.action_space.n
 	n_obs = env.observation_space.shape[0]
-	allowed_actions = [0, 2]
-
-	DQN_Options.SCALE_OBSERVATIONS = True
-	DQN_Options.SCALING_LIST = [100, 10000]
-
-	if mode == 'test':
-		DQN_Options.CURRENT_EPSILON = 0
-
 	sess = tf.InteractiveSession()
 	dqn = DQN(sess, env, n_actions, n_obs)
+	dqn.dqn_options.SCALE_OBSERVATIONS = True
+	dqn.dqn_options.SCALING_LIST = [100, 10000]
 	sess.run(tf.global_variables_initializer())
 
 	if mode == 'train':
@@ -52,4 +32,5 @@ if __name__ == '__main__':
 		dqn.train(2000, -200, action_set='reduced', allowed_actions=[0, 2], save_path='tmp_MC', save_freq=100)
 
 	elif mode == 'test':
+		dqn.dqn_options.CURRENT_EPSILON = 0
 		dqn.test('tmp_MC')
